8cfec553a29ea001728e7eb96977a5d40990ffde
adisamicheal <adisamicheal20@gmail.com>
2023-07-10 00:14:24 +0100

    update: improve type check and error handling

--- controllers/urlController.ts
+++ controllers/urlController.ts
@@ -3,19 +3,21 @@ import shortid from "shortid"
 import utils from "../utils/util"
 
 export const getAllSavedUrls = async (req: any, res: any, next: any) => {
-  console.log('called');
-
-  const url = await UrlModel.find();
-  
-  if (!url) {
-    return res.status(404).json({message: "No urls found"});
+  try {
+    const url = await UrlModel.find();
+    
+    if (!url) {
+      return res.status(404).json({message: "No URLs found"});
+    }
+    return res.status(200).json({url});
+  } catch (error) {
+    if (error instanceof Error) {
+      error.message ? console.error(error.message) : console.error(error);
+    }
   }
-  console.log(url);
-  return res.status(200).json({url});
 }
 
 export const shortenURL = async (req: any, res: any, next: any) => {
-  console.log("url shorten request", req.body.url);
   const { url } = req.body;
   const base = process.env.API_URL;
 
@@ -24,11 +26,8 @@ export const shortenURL = async (req: any, res: any, next: any) => {
     try {
       let urlVal = await UrlModel.findOne({ url });
       if (urlVal) {
-        console.log("reurn here?", url);
-        
-        return res.json(url);
+        return res.json(urlVal);
       }
-      console.log("reurn here instead?", url);
       const shortUrl = `${base}/${urlId}`;
 
       urlVal = new UrlModel({
@@ -38,13 +37,12 @@ export const shortenURL = async (req: any, res: any, next: any) => {
         // date: new Date()
       });
 
-      console.log("url value", urlVal);
-      
-
       await urlVal.save();
       return res.json(urlVal);
     } catch (error) {
-      console.error(error);
+      if (error instanceof Error) {
+        error.message ? console.error(error.message) : console.error(error);
+      }
       res.status(500).json({message: "Server Error"})
     }
   } else {
@@ -53,21 +51,20 @@ export const shortenURL = async (req: any, res: any, next: any) => {
 };
 
 export const redirectUrl = async (req: any, res: any, next: any) => {
-  console.log('gets here', req.params);
-  
   try {
     const urlVal = await UrlModel.findOne({ urlId: req.params.urlId})
-    console.log('urlVal', urlVal);
 
     if (urlVal) {
       urlVal.clicks+=1;
       urlVal.save();
       return res.redirect(urlVal.url);
     }
-    return res.status(404).json({message: "Not found"})
+    return res.status(404).json({message: "URL not found"})
     
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) {
+      error.message ? console.error(error.message) : console.error(error);
+    }
     return res.status(500).json({message: "Server Error"})
   }
 }
