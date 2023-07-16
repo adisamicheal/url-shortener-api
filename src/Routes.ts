5739f0dd953d870eadfa241b093cc521a877afe2
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-16 14:01:43 -0600

    Added: move to src + refactored

---
+++ src/Routes.ts
@@ -0,0 +1,13 @@
+import { Express } from "express";
+import Shorten_Url_Handler from "./Controllers/Su.Controller";
+
+const routes = (app: Express) => {
+  /*Api-Get*/
+  app.get("/api/get-all", Shorten_Url_Handler.Get_All_Urls); //get all shortened urls (Probably should secure)
+  app.get("/:url_Id", Shorten_Url_Handler.Redirect_Url); //redirect with a url
+
+  /*Api-Posts*/
+  app.post("/api/shorten-url", Shorten_Url_Handler.Create_Shorten_Url); //create a shorten url
+};
+
+export default routes;
