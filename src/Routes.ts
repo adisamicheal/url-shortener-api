ec9822d23b4e74c6a1b9bf06cd5cc362b83339b5
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-17 22:16:40 -0400

    Added: Type to Route function

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,7 +1,8 @@
 import { Express } from "express";
 import Shorten_Url_Handler from "./Controllers/Su.Controller";
+import { Routes_Type } from "./Types";
 
-const routes = (app: Express) => {
+const routes: Routes_Type = (app: Express) => {
   /*Api-Get*/
   app.get("/api/get-all", Shorten_Url_Handler.Get_All_Urls); //get all shortened urls (Probably should secure)
   app.get("/:url_Id", Shorten_Url_Handler.Redirect_Url); //redirect with a url
