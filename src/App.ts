e236acf152f2d6aa5dd672e5a676dfad6008a8a6
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-17 22:14:37 -0400

    Changed: renamed connect to db function name

--- src/App.ts
+++ src/App.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import routes from "./Routes";
-import connectToDatabase from "./Provider/Db.Provider";
+import dotenv from "dotenv";
+import connect_To_Db from "./Provider/Db.Provider";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.APP_PORT;
 
 app.listen(PORT, async () => {
-  console.log(await connectToDatabase());
+  console.log(await connect_To_Db());
   console.log(`Running on Localhost:${PORT}`);
   routes(app);
 });
